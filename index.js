a1954f36a919b83a5f4f4c87b59dd36535db4f0c
Basir <basir.jafarzadeh@gmail.com>
2019-04-09 15:44:39 -0700

    first

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+var path = require('path');
 
 const app = express();
 
@@ -10,7 +11,10 @@ app.listen(4000, () => {
 	console.log('Server Works !!! At port 4000');
 });
 
-app.get('/download', (req,res) => {
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/download', (req, res) => {
 	var URL = req.query.URL;
 	res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 	ytdl(URL, {
